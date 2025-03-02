81d058240d4fa9c3f961e4bbad6bca0d02e314fd
Ramya Harshini V S <ramyaaharshinivs8357@gmail.com>
2025-03-02 16:51:17 +0530

    Create MainJ.java

---
+++ src/main/java/com/example/MainJ.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class MainJ {
+    public String greet() {
+        return "Hello, Jenkins!"; 
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Main().greet()); 
+    }
+}
