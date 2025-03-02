6db38ee82fe12cbfba39bf7ec84f87ac80dc62c2
Ramya Harshini V S <ramyaaharshinivs8357@gmail.com>
2025-03-02 18:41:39 +0530

    Update MainJ.java

--- src/main/java/com/example/MainJ.java
+++ src/main/java/com/example/MainJ.java
@@ -6,6 +6,6 @@ public class MainJ {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Main().greet()); 
+        System.out.println(new MainJ().greet()); 
     }
 }
