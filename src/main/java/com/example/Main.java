16d2bfe8d365c10017d6037fa01c5e75eb7e6938
Ramya Harshini V S <ramyaaharshinivs8357@gmail.com>
2025-03-02 18:45:35 +0530

    Update and rename MainJ.java to Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,11 +1,11 @@
 package com.example;
 
-public class MainJ {
+public class Main {
     public String greet() {
         return "Hello, Jenkins!"; 
     }
 
     public static void main(String[] args) {
-        System.out.println(new MainJ().greet()); 
+        System.out.println(new Main().greet()); 
     }
 }
